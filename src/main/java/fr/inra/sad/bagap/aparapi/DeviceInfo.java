c047e572e597f89f92599e47188e5209de76c3c8
hboussard <hugues.boussard@inra.fr>
2019-10-03 16:17:31 +0200

    first commit

---
+++ src/main/java/fr/inra/sad/bagap/aparapi/DeviceInfo.java
@@ -0,0 +1,18 @@
+package fr.inra.sad.bagap.aparapi;
+
+import com.aparapi.device.Device;
+import com.aparapi.internal.kernel.KernelManager;
+import com.aparapi.internal.kernel.KernelPreferences;
+
+public class DeviceInfo {
+	
+	public static void main(String[] args) {
+		KernelPreferences preferences = KernelManager.instance().getDefaultPreferences();
+		System.out.println(KernelManager.instance().bestDevice());
+		System.out.println("-- Devices in preferred order --");
+		for (Device device : preferences.getPreferredDevices(null)) {
+			System.out.println("----------");
+			System.out.println(device);
+		}
+	}
+}
