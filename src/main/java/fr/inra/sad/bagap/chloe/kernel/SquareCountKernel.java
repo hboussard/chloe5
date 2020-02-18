b35e60e1f8a0808affe4f2fd08a4a42a5d4f41bb
Paul Meurice <paul.meurice@inra.fr>
2020-02-18 16:12:32 +0100

    SquareCountKernel et GaussianWeightedCountKernel : correction de bugs, ajout enveloppeInterne, changement constructeur de values à nValues

--- src/main/java/fr/inra/sad/bagap/chloe/kernel/SquareCountKernel.java
+++ src/main/java/fr/inra/sad/bagap/chloe/kernel/SquareCountKernel.java
@@ -16,91 +16,81 @@ public class SquareCountKernel extends Kernel {
 	
 	private final int rayon;
 	
-	private final short[] values;
+	private final int nValues;
 	
 	private int theY;
 
 	private final float[][] buf;
 	
-	private final int noDataValue;
-	
 	private final int enveloppeInterne; // in pixels
 	
-	public SquareCountKernel(short[] values, int windowSize, int width, int height, int dep, float[] imageIn, float[][] imageOut, int noDataValue){
-		this(values, windowSize, width, height, dep, imageIn, imageOut, noDataValue, 0);
+	public SquareCountKernel(int nValues, int windowSize, int width, int height, int dep, float[] imageIn, float[][] imageOut){
+		this(nValues, windowSize, width, height, dep, imageIn, imageOut, 0);
 	}
 		
 	
-	public SquareCountKernel(short[] values, int windowSize, int width, int height, int dep, float[] imageIn, float[][] imageOut, int noDataValue, int enveloppeInterne){
+	public SquareCountKernel(int nValues, int windowSize, int width, int height, int dep, float[] imageIn, float[][] imageOut, int enveloppeInterne){
 		this.setExplicit(true);
 		this.setExecutionModeWithoutFallback(Kernel.EXECUTION_MODE.JTP);
-		this.values = values;
+		this.nValues = nValues;
 		this.rayon = windowSize/2 + 1;
 		this.width = width;
 		this.height = height;
 		this.dep = dep;
 		this.imageIn = imageIn;
 		this.imageOut = imageOut;
-		this.noDataValue = noDataValue;
 		this.enveloppeInterne = enveloppeInterne;
-		this.buf = new float[width][values.length+2];
+		this.buf = new float[width][nValues];
 
 	}
 
-	public void processVerticalPixel(int x,int line) {
-		int y = theY + line;
-		int i,dy;
-		// parcours vertical de l'image centré sur ligne theY+line et stocké dans buf
-		for(i=0;i<values.length;i++) {
+	public void processVerticalPixel(int x,int y) {
+		// parcours vertical centré sur (x,y) dans imageIn et stocké dans buf
+		for(int i=0;i<nValues;i++)
 			buf[x][i]=0;
-		}
-		for (dy = -rayon+1; dy < rayon; dy++) {
-			if(((y + dy) >= 0) && ((y + dy) < height)){
-				
-				int ic = abs(dy);
-				int v = (int)imageIn[((y + dy) * width) + x];
-				buf[x][v] ++;	
-
-			}
+		
+		for (int dy = max(-rayon+1,-y); dy < min(rayon,height-y); dy++) {
+				int v = (int)imageIn[(y + dy) * width + x];
+				buf[x][v]++;	
 		}
 	}
 
-	public void processHorizontalPixel(int x,int line) {
+	public void processHorizontalPixel(int x_out,int y_out) {
 
-		// parcours horizontal de buf: position dans imageOut : (x,line/dep)
-		int x_buf = x*dep;
-		int y=line;
-		int ind = y*((width-1)/dep+1) + x;
-		int i;
-		float val;
+		// parcours horizontal de buf: position dans imageOut : (x_in/dep,y_out)
+		int x_in = x_out*dep;
+		int ind = y_out*((width-1)/dep+1) + x_out;
 		
-		for(int value=0; value<values.length;value++) {
-			val=0;
-			for(i=max(x_buf-rayon+1,0);i<min(x_buf+rayon,width);i++) {
+		for(int value=0; value<nValues;value++) {
+			float val=0;
+			for(int i=max(x_in-rayon+1,0);i<min(x_in+rayon,width);i++)
 				val+=buf[i][value];
-			}
 			imageOut[ind][value] = val;
 		}
+	//	System.out.println("x="+x_out+" , y="+y_out+" , "+imageOut[ind][1]+" , "+imageOut[ind][2]);
 
 	}
 
 
 	public void applySlidingWindow(int theY, short buffer) {
-		this.theY = theY;
+		this.theY = theY; // ligne dans 
 		int nlines = (buffer - ((dep-theY%dep)%dep) )/dep;
 		execute(width,2*nlines);
 	}
 
 	@Override
 	public void run() {
-		int x = getGlobalId(0);
+		int x = getGlobalId(0); // x dans imageIn
 		int pass = getPassId();
 		int vertical = pass%2;
-		int line= (dep-theY%dep)%dep + pass/2;
+		int line= (dep-theY%dep)%dep + pass/2; // ligne dans imageOut
+		int y = theY + line*dep; // y dans imageIn
+		if(y<enveloppeInterne || y>=height-enveloppeInterne)
+			return;
 		if(vertical == 0)
-			processVerticalPixel(getGlobalId(0),line);
+			processVerticalPixel(x,y);
 		else {
-			if( x < (width-1)/dep+1 )
+			if(x>=enveloppeInterne/dep && x < (width-enveloppeInterne-1)/dep+1 )
 				processHorizontalPixel(x,line);
 		}
 	}
