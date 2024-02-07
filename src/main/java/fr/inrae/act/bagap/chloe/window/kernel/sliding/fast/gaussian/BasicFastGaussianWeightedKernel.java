919a8f01ff076f58337586b2a0e5354cbe48e7b4
Paul Meurice <paul.meurice@inrae.fr>
2024-02-07 10:53:22 +0100

    FAST : nouveau sigma pour pondération gaussienne

--- src/main/java/fr/inrae/act/bagap/chloe/window/kernel/sliding/fast/gaussian/BasicFastGaussianWeightedKernel.java
+++ src/main/java/fr/inrae/act/bagap/chloe/window/kernel/sliding/fast/gaussian/BasicFastGaussianWeightedKernel.java
@@ -11,11 +11,9 @@ public class BasicFastGaussianWeightedKernel extends FastQualitativeKernel {
 
 		this.gauss = new float[rayon()];
 		
-		float r=(rayon()-1)/2.f; // sigma = r/sqrt(2), rayon = 2*sqrt(2)*sigma
-		for(int i=0; i<rayon(); i++) {
-			float d = i/r; // distance en nombre de rayons
-			gauss[i] = (float) Math.exp(-d*d);
-		}
+		float sigma = rayon()/sqrt(log(2)); // de telle sorte que le diametre correspondant en threshold = sqrt(2)*FWHM
+		for(int i=0; i<rayon(); i++) 
+			gauss[i] = (float) Math.exp(-i*i/(2*sigma*sigma));
 	}
 	
 	@Override
